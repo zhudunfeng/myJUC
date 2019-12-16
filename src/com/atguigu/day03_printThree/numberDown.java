e9ee30e9cffd5d1a492d261eb5dd41a09613ae4c
朱敦峰 <2101580047@qq.com>
2019-12-16 17:56:05 +0800

    12.16 17:55

--- src/com/atguigu/day03_printThree/numberDown.java
+++ src/com/atguigu/day03_printThree/numberDown.java
@@ -1,8 +1,10 @@
 package com.atguigu.day03_printThree;
 
-public class CurrrentDown {
+import java.util.concurrent.CountDownLatch;
+
+public class numberDown {
     public static void main(String[] args) {
-        
+        CountDownLatch countDownLatch = new CountDownLatch(7);
         for (int i = 1; i <=10 ; i++) {
                     new Thread(()->{
 
