2debc930de6ccfa7e4a61ee0c2c1f0d3e0c6e4f8
朱敦峰 <2101580047@qq.com>
2019-12-17 11:19:30 +0800

    12.17-11:19master

--- src/com/atguigu/day03_printThree/BlockingQueueDemo.java
+++ src/com/atguigu/day03_printThree/BlockingQueueDemo.java
@@ -3,6 +3,9 @@ package com.atguigu.day03_printThree;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 线程池
+ */
 public class BlockingQueueDemo {
     public static void main(String[] args) {
        List list=new ArrayList();
