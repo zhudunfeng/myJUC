b55c13b33ba0bab0bf316af59c73ffd6a3778eb6
zhudunfeng <2101580047@qq.com>
2021-05-21 00:20:07 +0800

    循环依赖案例

---
+++ src/com/adun/test_Circular_dependency/constructor/ServiceB.java
@@ -0,0 +1,8 @@
+package com.adun.test_Circular_dependency.constructor;
+
+/**
+ * @author zhudunfeng
+ * @date 2021/5/21 0:10
+ */
+public class ServiceB {
+}
---
+++ test_Spring/src/test/java/com/adun/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
