e5a80cbf61a17f2ded1fde04a630161fe12a3827
zhudunfeng <2101580047@qq.com>
2025-03-19 22:39:40 +0800

    feat: springboot整合通用责任链

---
+++ design_mode/design_mode_chain04/src/test/java/com/adun/DesignModeChain03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignModeChain03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
